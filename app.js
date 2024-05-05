f8a0d3bf21e369d1aa69edf70d28ec727fe4298e
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-05-05 22:56:12 +0700

    feat: separate route for Review module

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
 const port = 3000;
 
-// Models
-const Review = require("./models/review");
-
-// Schema
-const { reviewSchema } = require("./schemas/review");
-
 // Connect to mongodb
 mongoose
   .connect("mongodb://127.0.0.1/wave_whisper")
@@ -31,46 +25,12 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    return next(new ErrorHandler(msg, 400));
-  } else {
-    next();
-  }
-};
-
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.use("/beaches", require("./routes/beaches"));
-
-app.post(
-  "/beaches/:id/reviews",
-  validateReview,
-  asyncHandler(async (req, res) => {
-    const review = new Review(req.body.review);
-    const beach = await Beach.findById(req.params.id);
-    beach.reviews.push(review);
-    await review.save();
-    await beach.save();
-    res.redirect(`/beaches/${req.params.id}`);
-  })
-);
-
-app.delete(
-  "/beaches/:beach_id/reviews/:review_id",
-  asyncHandler(async (req, res) => {
-    const { beach_id, review_id } = req.params;
-    await Beach.findByIdAndUpdate(beach_id, {
-      $pull: { reviews: review_id },
-    });
-    await Review.findByIdAndDelete(review_id);
-    res.redirect(`/beaches/${beach_id}`);
-  })
-);
+app.use("/beaches/:beach_id/reviews", require("./routes/reviews"));
 
 app.all("*", (req, res, next) => {
   next(new ErrorHandler("Page not found", 404));
---
+++ routes/reviews.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const Beach = require("../models/beach");
+const Review = require("../models/review");
+const { reviewSchema } = require("../schemas/review");
+const ErrorHandler = require("../utils/ErrorHandler");
+const asyncHandler = require("../utils/asyncHandler");
+
+const router = express.Router({ mergeParams: true });
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    return next(new ErrorHandler(msg, 400));
+  } else {
+    next();
+  }
+};
+
+router.post(
+  "/",
+  validateReview,
+  asyncHandler(async (req, res) => {
+    const review = new Review(req.body.review);
+    const beach = await Beach.findById(req.params.beach_id);
+    beach.reviews.push(review);
+    await review.save();
+    await beach.save();
+    res.redirect(`/beaches/${req.params.beach_id}`);
+  })
+);
+
+router.delete(
+  "/:review_id",
+  asyncHandler(async (req, res) => {
+    const { beach_id, review_id } = req.params;
+    await Beach.findByIdAndUpdate(beach_id, {
+      $pull: { reviews: review_id },
+    });
+    await Review.findByIdAndDelete(review_id);
+    res.redirect(`/beaches/${beach_id}`);
+  })
+);
+
+module.exports = router;
