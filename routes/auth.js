48766327e70ac1f118e017c9ddc8b6b018056133
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-05-06 10:05:41 +0700

    feat: login

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const asyncHandler = require("../utils/asyncHandler");
+const passport = require("passport");
 
 router.get("/register", (req, res) => {
   res.render("auth/register");
@@ -21,6 +22,25 @@ router.post(
       res.redirect("/register");
     }
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: {
+      type: "error_msg",
+      msg: "Invalid username or password",
+    },
+  }),
+  (req, res) => {
+    req.flash("success_msg", "You are logged in");
+    res.redirect("/beaches");
+  }
 );
 
 module.exports = router;
