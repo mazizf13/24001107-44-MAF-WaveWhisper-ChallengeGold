48e3311fa831dc21a277beb0cfa62a5899694b07
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-05-06 07:37:16 +0700

    feat: create id validation middleware

---
+++ middleware/isValidObjectId.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+module.exports = (redirectUrl = "/") => {
+  return async (req, res, next) => {
+    const paramId = ["id", "beach_id", "review_id"].find(
+      (param) => req.params[param]
+    );
+
+    if (!paramId) {
+      return next();
+    }
+
+    const id = req.params[paramId];
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      req.flash("error_msg", "invalid Id / Data tidak ditemukan");
+      return res.redirect(redirectUrl);
+    }
+
+    next();
+  };
+};
