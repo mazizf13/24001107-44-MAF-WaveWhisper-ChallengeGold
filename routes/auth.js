d5efe99acaca99fabf26d1ebb2927e458e9278b1
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-05-06 12:05:44 +0700

    feat: logout

--- routes/auth.js
+++ routes/auth.js
@@ -43,4 +43,14 @@ router.post(
   }
 );
 
+router.post("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success_msg", "You are logged out");
+    res.redirect("/beaches");
+  });
+});
+
 module.exports = router;
